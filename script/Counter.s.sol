b7f58f24749aac4f6c62feaec9846acb1b086aaa
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-05-02 17:42:22 +0300

    chore: forge init

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
-import {Contract} from "src/Contract.sol";
-
-contract ContractScript is Script {
+contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
-        new Contract();
     }
 }
--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,36 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
-contract Contract {
-    constructor() {}
+contract SimpleCounter {
+    uint256 public counter;
+    address public owner;
+    constructor() {
+      counter = 0;
+      owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+      require(msg.sender == owner, "Only owner can call this function");
+      _;
+    }
+
+    function increment() public onlyOwner {
+      counter++;
+    }
+
+    function decrement() public onlyOwner {
+      require(counter > 0 , "Counter cannot be negative");
+      counter--;
+    }
+
+    function reset() public onlyOwner {
+      counter = 0;
+    }
+
+    function getCounter() public view returns (uint256)  {
+      return counter;
+    }
+
 }
+
+
